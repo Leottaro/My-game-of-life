641c1c0c7b91f7fdd6844667dd86245b3cca5c0e
Leottaro <94469101+Leottaro@users.noreply.github.com>
2022-07-19 18:54:35 +0200

    Add files via upload

---
+++ Game of life/Game of Life.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Game_Of_Life
+{
+	public class GameOfLife
+	{
+		private bool[,] Grid;
+		public int Width { get; }
+		public int Height { get; }
+
+		public GameOfLife(int width, int height)
+        {
+			Grid = new bool[height, width];
+			Width = width;
+			Height = height;
+        }
+
+		public void changeCell(int y, int x) { Grid[y, x] = !Grid[y, x]; }
+		public void setCell(int y, int x, bool color) { Grid[y, x] = color; }
+		public bool getCell(int y, int x) { return Grid[y, x]; }
+
+		public List<(int, int)> nextGen()
+        {
+			List<(int, int)> changed = new List<(int, int)>();
+			for(int y = 0; y < Height; y++)
+            {
+				for (int x = 0; x < Width; x++)
+				{
+                    int score = 0;
+
+					if (x >= 1)
+                    {
+						if (Grid[y, x - 1]) 
+							score++;
+						if (y >= 1 && Grid[y - 1, x - 1]) 
+							score++;
+						if (y + 1 < Height && Grid[y + 1, x - 1]) 
+							score++;
+					}
+					if (x + 1 < Width)
+                    {
+						if (Grid[y, x + 1]) 
+							score++;
+						if (y >= 1 && Grid[y - 1, x + 1]) 
+							score++;
+						if (y < Height - 1 && Grid[y + 1, x + 1]) 
+							score++;
+					}
+					if (y >= 1 && Grid[y - 1, x]) 
+						score++;
+					if (y < Height - 1 && Grid[y + 1, x]) 
+						score++;
+
+					if ((Grid[y, x] && (score < 2 || score > 3)) || (!Grid[y, x] && score == 3)) 
+						changed.Add((y, x));
+                } 
+			}
+			foreach((int y, int x) in changed) 
+				Grid[y, x] = !Grid[y, x];
+			return changed;
+        }
+	}
+}
--- Game of life/MainWindow.xaml.cs
+++ Game of life/MainWindow.xaml.cs
@@ -1,33 +1,34 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows.Threading;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
 
-namespace Game_Of_LIfe
+namespace Game_Of_Life
 {
     public partial class MainWindow : Window
     {
-        const float resize = 2 / 3f;
-        public int[,] Grid = new int[0, 0];
-        public int GridWidth = 10;
-        public int GridHeight = 10;
-        public int CellSize = 10;
-        public bool isOn = false;
-        public DispatcherTimer timer = new DispatcherTimer();
-        public int tick = 100;
-        public int gen = 0;
+        GameOfLife Game;
+        public int CellSize;
+        public bool isOn;
+        public int tick;
+        public int gen;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            tick = 30;
-            CellSize = 10;
-            GridWidth = 10;
-            GridHeight = 10;
+            CellSize = 16;
+            isOn = false;
+            tick = 1000/30;
+            gen = 0;
 
+            float resize = 2 / 3f;
             MyCanvas.Width = CellSize * (int)((System.Windows.SystemParameters.PrimaryScreenWidth * resize) / CellSize);
             MyCanvas.Height = CellSize * (int)((System.Windows.SystemParameters.PrimaryScreenHeight * resize) / CellSize);
 
@@ -43,7 +44,7 @@ namespace Game_Of_LIfe
             PauseButton.Margin = new Thickness(MyCanvas.Width - ClearButton.Width - PauseButton.Width, MyCanvas.Height, 0, 0);
             PauseButton.Content = "Play";
 
-            LabelTick.Content = tick.ToString() + "ms";
+            LabelTick.Content = String.Format("{0}ms", tick);
             LabelTick.Width = 60;
             LabelTick.Height = 30;
             LabelTick.Margin = new Thickness(0, MyCanvas.Height, 0, 0);
@@ -53,13 +54,10 @@ namespace Game_Of_LIfe
             TickSlider.Height = 30;
             TickSlider.Margin = new Thickness(LabelTick.Width + 5, MyCanvas.Height + 5, 0, 0);
 
-            GridWidth = (int)(MyCanvas.Width / CellSize);
-            GridHeight = (int)(MyCanvas.Height / CellSize);
-
-            Grid = new int[GridHeight, GridWidth];
-            for (int y = 0; y < GridHeight; y++)
+            Game = new GameOfLife((int)(MyCanvas.Width / CellSize), (int)(MyCanvas.Height / CellSize));
+            for (int y = 0; y < Game.Height; y++)
             {
-                for (int x = 0; x < GridWidth; x++)
+                for (int x = 0; x < Game.Width; x++)
                 {
                     Rectangle rect = new Rectangle
                     {
@@ -70,112 +68,33 @@ namespace Game_Of_LIfe
                         Fill = Brushes.White,
                         Stroke = Brushes.Black
                     };
-                    Grid[y, x] = 0;
                     MyCanvas.Children.Add(rect);
                 }
             }
-
-            timer.Tick += new EventHandler(TimerTick);
-            timer.Interval = new TimeSpan(0, 0, 0, 0, tick);
         }
 
-        public Rectangle GetRect(int x, int y)
+        public Rectangle GetRect(int y, int x) { return (Rectangle)(MyCanvas.Children[y * Game.Width + x + 5]); }
+        
+        public async void loop()
         {
-            return (Rectangle)(MyCanvas.Children[y * GridWidth + x + 5]);
-        }
-
-        private void TimerTick(object sender, EventArgs e)
-        {
-            var nextgen = new (int, int)[GridWidth * GridHeight];
-            int i = 0;
-
-            for (int y = 0; y < GridHeight; y++)
+            while (isOn)
             {
-                for (int x = 0; x < GridWidth; x++)
-                {
-                    int score = 0;
-
-                    if (x > 0)
-                    {
-                        if (y > 0)
-                        {
-                            if (Grid[y - 1, x - 1] == 1) { score++; }
-                        }
-                        if (y < GridHeight - 1)
-                        {
-                            if (Grid[y + 1, x - 1] == 1) { score++; }
-                        }
-                        if (Grid[y, x - 1] == 1) { score++; }
-                    }
-                    if (x < GridWidth - 1)
-                    {
-                        if (y > 0)
-                        {
-                            if (Grid[y - 1, x + 1] == 1) { score++; }
-                        }
-                        if (y < GridHeight - 1)
-                        {
-                            if (Grid[y + 1, x + 1] == 1) { score++; }
-                        }
-                        if (Grid[y, x + 1] == 1) { score++; }
-                    }
-                    if (y > 0)
-                    {
-                        if (Grid[y - 1, x] == 1) { score++; }
-                    }
-                    if (y < GridHeight - 1)
-                    {
-                        if (Grid[y + 1, x] == 1) { score++; }
-                    }
-
-                    if (Grid[y, x] == 1)
-                    {
-                        if (score < 2 || score > 3)
-                        {
-                            nextgen[i] = (x, y);
-                            i++;
-                        }
-                    }
-                    else
-                    {
-                        if (score == 3)
-                        {
-                            nextgen[i] = (x, y);
-                            i++;
-                        }
-                    }
-                }
-            }
-
-            if (i == 0) { isOn = false; }
-
-            for (int j = 0; j < i; j++)
-            {
-                (int x, int y) = nextgen[j];
-
-                if (Grid[y, x] == 1)
-                {
-                    Grid[y, x] = 0;
-                    GetRect(x, y).Fill = Brushes.White;
-                }
-                else
-                {
-                    Grid[y, x] = 1;
-                    GetRect(x, y).Fill = Brushes.Black;
-                }
-            }
-
-            GenLabel.Content = gen.ToString();
-            if (isOn)
-            {
+                List<(int, int)> changed = Game.nextGen();
+                if (changed.Count == 0) isOn = false;
+                foreach ((int y, int x) in changed)
+                    GetRect(y, x).Fill = Game.getCell(y, x) ? Brushes.Black : Brushes.White;
                 gen += 1;
-                timer.Start();
+                GenLabel.Content = String.Format("gen {0}", gen);
+                await Task.Delay(tick);
             }
-            else
-            {
-                timer.Stop();
-                PauseButton.Content = "Play";
-            }
+            pause();
+        }
+
+        public void pause()
+        {
+            gen = 0;
+            GenLabel.Content = "gen 0";
+            PauseButton.Content = "Play";
         }
 
         private void OnKeyDown(object sender, KeyEventArgs e)
@@ -185,40 +104,27 @@ namespace Game_Of_LIfe
                 if (isOn)
                 {
                     isOn = false;
-                    PauseButton.Content = "Play";
                 }
                 else
                 {
                     isOn = true;
-                    timer.Start();
                     PauseButton.Content = "Pause";
+                    loop();
                 }
             }
             else if (e.Key == Key.Escape)
-            {
                 MyWindow.Close();
-            }
             if (e.Key == Key.C || e.Key == Key.R)
             {
                 isOn = false;
-                PauseButton.Content = "Play";
-                timer.Stop();
-                GenLabel.Content = "0";
-                gen = 0;
-
-                Grid = new int[GridHeight, GridWidth];
-                for (int y = 0; y < GridHeight; y++)
-                {
-                    for (int x = 0; x < GridWidth; x++)
-                    {
-                        Grid[y, x] = 0;
-                        GetRect(x, y).Fill = Brushes.White;
-                    }
-                }
+                Game = new GameOfLife(Game.Width, Game.Height);
+                for (int y = 0; y < Game.Height; y++)
+                    for (int x = 0; x < Game.Width; x++)
+                        GetRect(y, x).Fill = Brushes.White;
             }
         }
 
-        private void OnClick(object sender, MouseButtonEventArgs e)
+        private void MyCanvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (!isOn && e.OriginalSource is Rectangle)
             {
@@ -228,12 +134,12 @@ namespace Game_Of_LIfe
 
                 if (e.LeftButton == MouseButtonState.Pressed && rect.Fill == Brushes.White)
                 {
-                    Grid[y, x] = 1;
+                    Game.setCell(y, x, true);
                     rect.Fill = Brushes.Black;
                 }
                 if (e.RightButton == MouseButtonState.Pressed && rect.Fill == Brushes.Black)
                 {
-                    Grid[y, x] = 0;
+                    Game.setCell(y, x, false);
                     rect.Fill = Brushes.White;
                 }
             }
@@ -249,12 +155,12 @@ namespace Game_Of_LIfe
 
                 if (e.LeftButton == MouseButtonState.Pressed && rect.Fill == Brushes.White)
                 {
-                    Grid[y, x] = 1;
+                    Game.setCell(y, x, true);
                     rect.Fill = Brushes.Black;
                 }
                 if (e.RightButton == MouseButtonState.Pressed && rect.Fill == Brushes.Black)
                 {
-                    Grid[y, x] = 0;
+                    Game.setCell(y, x, false);
                     rect.Fill = Brushes.White;
                 }
             }
@@ -263,8 +169,7 @@ namespace Game_Of_LIfe
         private void TickSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             tick = (int)(e.NewValue);
-            LabelTick.Content = tick.ToString() + "ms";
-            timer.Interval = new TimeSpan(0, 0, 0, 0, tick);
+            LabelTick.Content = String.Format("{0}ms", tick.ToString());
         }
 
         private void PauseButton_Click(object sender, RoutedEventArgs e)
@@ -272,32 +177,22 @@ namespace Game_Of_LIfe
             if (isOn)
             {
                 isOn = false;
-                PauseButton.Content = "Play";
             }
             else
             {
                 isOn = true;
                 PauseButton.Content = "Pause";
-                timer.Start();
+                loop();
             }
         }
 
         private void ClearButton_Click(object sender, RoutedEventArgs e)
         {
             isOn = false;
-            timer.Stop();
-            GenLabel.Content = "0";
-            gen = 0;
-
-            Grid = new int[GridHeight, GridWidth];
-            for (int y = 0; y < GridHeight; y++)
-            {
-                for (int x = 0; x < GridWidth; x++)
-                {
-                    Grid[y, x] = 0;
-                    GetRect(x, y).Fill = Brushes.White;
-                }
-            }
+            Game = new GameOfLife(Game.Width, Game.Height);
+            for (int y = 0; y < Game.Height; y++)
+                for (int x = 0; x < Game.Width; x++)
+                    GetRect(y, x).Fill = Brushes.White;
         }
     }
-}
+} 
